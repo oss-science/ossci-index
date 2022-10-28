7a18354a95e7a52d2da3650e7a05e293d134a99f
Harry Stavropoulos <harryst@gmail.com>
2022-10-28 17:17:41 -0400

    Added find_dependency_leaves.py - which prints the packages with no dependencies

---
+++ dependency-discovery/python/sourcegraph/find_dependency_leaves.py
@@ -0,0 +1,40 @@
+from typing import Dict
+import argparse
+import sys
+import os
+
+
+def _main(argv = None):
+    if argv is None:
+        argv = sys.argv
+
+    parser = argparse.ArgumentParser(argv[0], description = 'Looks in the dir which contains several files with contents à la "package,dependency".' 
+    + ' Prints out the packages which have NO dependencies')
+    parser.add_argument('dependecyFilesDir', type = str, help = 'Path to the dir containing the dependency files')
+    args = parser.parse_args(args = argv[1: ])
+
+    dependencies: Dict[str, set[str]] = {}
+    for filename in os.listdir(args.dependecyFilesDir):
+        if filename.lower() == 'readme.md':
+            continue
+
+        filepath = os.path.join(args.dependecyFilesDir, filename)
+        if os.path.isfile(filepath):
+            with open(filepath, 'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    [down, up] = line.split(',')
+
+                    if down not in dependencies:
+                        dependencies[down] = set()
+
+                    dependencies[down].add(up.strip())
+    
+    for dependencySet in dependencies.values():
+        for up in dependencySet:
+            if up not in dependencies:
+                print(up)
+                
+
+if __name__ == '__main__':
+    sys.exit(_main())
